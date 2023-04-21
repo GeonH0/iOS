1790be770fae114bfe856c6a8515d98c210a9579
GeonH0 <h2jo369@naver.com>
2023-04-22 01:36:06 +0900

    GoogleError

--- loginbySpotify/loginbySpotify/AppDelegate.swift
+++ loginbySpotify/loginbySpotify/AppDelegate.swift
@@ -7,21 +7,34 @@
 
 import UIKit
 import Firebase
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        // Firebase 초기화
         FirebaseApp.configure()
+        
+
+        // Firebase 초기화
+        
+        
+        
         
         
         return true
     }
+    
+    
+
+    
+    
 
     // MARK: UISceneSession Lifecycle
 
@@ -36,6 +49,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
 
 
 }
