beb661c391313a5dec05ed1b980549498cf06a83
GeonH0 <h2jo369@naver.com>
2023-06-19 23:47:45 +0900

    Remote Config

--- Noitce/Noitce/ViewController.swift
+++ Noitce/Noitce/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import FirebaseRemoteConfig
 
 class ViewController: UIViewController {
+    
+    var remoteConfig: RemoteConfig?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        remoteConfig = RemoteConfig.remoteConfig()
+        
+        let setting = RemoteConfigSettings()
+        setting.minimumFetchInterval = 0
+        //setting 설정 최대한 자주 구성할수 있도록 0으로 설정
+        
+        remoteConfig?.configSettings = setting
+        remoteConfig?.setDefaults(fromPlist: "RemoteConfigDefaults")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        getNotice()
     }
 
 
+}
+
+extension ViewController {
+    func getNotice() {
+        guard let remoteConfig = remoteConfig else {return}
+        
+        remoteConfig.fetch{ [weak self] status, _ in
+            if status == .success {
+                remoteConfig.activate(completion:nil)
+            } else {
+                print("ERROR: Config not fetched")
+            }
+            
+            guard let self = self else { return }
+            if !self.isNoticeHidden(remoteConfig){
+                let noticeVC = NoticeViewController(nibName: "NoticeViewController", bundle: nil)
+                
+                noticeVC.modalPresentationStyle = .custom
+                noticeVC.modalTransitionStyle = .crossDissolve
+                
+                let title = (remoteConfig["title"].stringValue ?? "").replacingOccurrences(of: "\\n", with: "\n")
+                let detail = (remoteConfig["detail"].stringValue ?? "").replacingOccurrences(of: "\\n", with: "\n")
+                
+                let date = (remoteConfig["date"].stringValue ?? "").replacingOccurrences(of: "\\n", with: "\n")
+                
+                noticeVC.noticeContents = (title: title,detail:detail,date:date)
+                self.present(noticeVC,animated: true,completion: nil)
+            }
+        }
+        
+    }
+    
+    func isNoticeHidden(_ remoteConfig: RemoteConfig) -> Bool {
+        return remoteConfig["isHidden"].boolValue
+    }
 }
 
