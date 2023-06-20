afc9b8388eb0ea140b467e75d7619747aa55879d
GeonH0 <h2jo369@naver.com>
2023-06-20 16:28:58 +0900

    local notification

---
+++ localNotification/localNotification/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  localNotification
+//
+//  Created by 김건호 on 2023/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
