e026aa36909994535081a92a2a9a45e018ff9f5c
GeonH0 <h2jo369@naver.com>
2023-07-26 13:30:09 +0900

    set BasicTypeSection

--- NetflicxStyleSampleApp/NetflicxStyleSampleApp/ContentCollectionViewHeader.swift
+++ NetflicxStyleSampleApp/NetflicxStyleSampleApp/ContentCollectionViewHeader.swift
@@ -2,28 +2,24 @@
 //  ContentCollectionViewHeader.swift
 //  NetflicxStyleSampleApp
 //
-//  Created by 김건호 on 2023/06/30.
+//  Created by 김건호 on 2023/07/26.
 //
 
 import UIKit
 
-class ContentCollectionViewHeader : UICollectionReusableView {
+class ContentCollectionViewHeader: UICollectionReusableView {
     let sectionNameLabel = UILabel()
-    
+        
     override func layoutSubviews() {
         super.layoutSubviews()
-        
         sectionNameLabel.font = .systemFont(ofSize: 17, weight: .bold)
         sectionNameLabel.textColor = .white
         sectionNameLabel.sizeToFit()
         
         addSubview(sectionNameLabel)
-        
-        sectionNameLabel.snp.makeConstraints{
+        sectionNameLabel.snp.makeConstraints {
             $0.centerY.equalToSuperview()
             $0.top.bottom.leading.equalToSuperview().offset(10)
         }
-        
     }
-    
 }
--- NetflicxStyleSampleApp/NetflicxStyleSampleApp/HomeViewController.swift
+++ NetflicxStyleSampleApp/NetflicxStyleSampleApp/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 
 class HomeViewController : UICollectionViewController {
@@ -22,6 +23,15 @@ class HomeViewController : UICollectionViewController {
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "netflix_icon"), style: .plain, target: nil, action: nil)
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.crop.circle"), style: .plain, target: nil, action: nil)
         
+        
+        //CollectionView 설정
+        collectionView.register(ContentCollectionViewCell.self, forCellWithReuseIdentifier: "ContentCollectionViewCell")
+        collectionView.register(ContentCollectionViewHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "ContentCollectionViewHeader")
+        
+        collectionView.collectionViewLayout = layout()
+        
+        
+        
         //set Data
         contents = getContents()
     }
@@ -34,18 +44,63 @@ class HomeViewController : UICollectionViewController {
               let list = try? PropertyListDecoder().decode([Content].self, from: data) else { return [] }
         return list
     }
+    //각각의 섹션 타입에 대한 UICollectionViewLayout 생성
+    private func layout() -> UICollectionViewLayout{
+        return UICollectionViewCompositionalLayout{[weak self] sectionNumber, environment -> NSCollectionLayoutSection? in
+            guard let self = self else { return nil }
+            switch self.contents[sectionNumber].sectionType {
+            case .basic:
+                return self.createBasicTypeSection()
+            default:
+                return nil
+            }
+            
+        }
+    }
+    
+    private func createBasicTypeSection() -> NSCollectionLayoutSection {
+        //item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3), heightDimension: .fractionalHeight(0.7))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 10, leading: 5, bottom: 0, trailing: 5)
+        //group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.9), heightDimension: .estimated(200))
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 3)
+        //section
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous //스크롤 행동 설정
+        section.contentInsets = .init(top: 0, leading: 5, bottom: 0, trailing: 5   )
+        let sectionHaeader = self.createSectionHeader()
+        section.boundarySupplementaryItems = [sectionHaeader]
+        
+        return section
+    }
+    
+    //section Header layout 설정
+    private func createSectionHeader() -> NSCollectionLayoutBoundarySupplementaryItem{
+        let layoutSectionHeaderSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(30))
+        //section Header Layout
+        let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: layoutSectionHeaderSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+        return sectionHeader
+    }
 }
 
 extension HomeViewController {
     //섹션당 보여질 셀의 개수
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        switch section {
-        case 0:
-            return 1
-        default:
-            return contents[section].contentItem.count
+        if contents[section].sectionType == .basic{
+            switch section {
+            case 0:
+                return 1
+            default:
+                return contents[section].contentItem.count
+            }
         }
+        return 0
     }
+    
     
     
     //콜렉션뷰 셀 설정
@@ -55,16 +110,20 @@ extension HomeViewController {
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ContentCollectionViewCell", for: indexPath) as? ContentCollectionViewCell else { return UICollectionViewCell() }
             cell.imageView.image = contents[indexPath.section].contentItem[indexPath.row].image
             return cell
-        case .main:
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ContentCollectionViewMainCell", for: indexPath) as? ContentCollectionViewMainCell else { return UICollectionViewCell() }
-            cell.imageView.image = mainItem?.image
-            cell.descriptionLabel.text = mainItem?.description
-            return cell
-        case .rank:
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ContentCollectionViewRankCell", for: indexPath) as? ContentCollectionViewRankCell else { return UICollectionViewCell() }
-            cell.imageView.image = contents[indexPath.section].contentItem[indexPath.row].image
-            cell.rankLabel.text = String(describing: indexPath.row + 1)
-            return cell
+
+        default:
+            return UICollectionViewCell()
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionHeader {
+            guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "ContentCollectionViewHeader", for: indexPath) as? ContentCollectionViewHeader else { fatalError("Could not dequeue Header") }
+            
+            headerView.sectionNameLabel.text = contents[indexPath.section].sectionName
+            return headerView
+        } else {
+            return UICollectionReusableView()
         }
     }
     
