0e14a6b1faf156f09db66823c07d707fe97406b0
GeonH0 <h2jo369@naver.com>
2023-07-29 23:12:13 +0900

    set app

--- Brewery/Brewery/BeerListViewController.swift
+++ Brewery/Brewery/BeerListViewController.swift
@@ -8,6 +8,18 @@
 import UIKit
 
 class BeerListViewController : UITableViewController{
+    var beerList = [Beer]()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //UiNavigationBar
+        title = "브루어리"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+}
+
+extension BeerListViewController {
     
 }
