036785c47758ba689d795de75597938843467cc2
GeonH0 <h2jo369@naver.com>
2024-01-21 23:22:50 +0900

    EXAMPLE 파일 추가

---
+++ EXAMPLE/EXAMPLE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EXAMPLE
+//
+//  Created by 김건호 on 1/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EXAMPLE/EXAMPLE/EXAMPLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  EXAMPLEApp.swift
+//  EXAMPLE
+//
+//  Created by 김건호 on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct EXAMPLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
